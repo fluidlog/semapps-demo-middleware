905969a94959e03e1df4eada7404b9a30dd65d8a
DUTHE <yannickduthe@hotmail.com>
2024-08-07 13:11:54 +0200

    Upgrade Archipelago 2.1.1

--- deploy-archipelago-glocal/addOn/frontend/src/resources/Agent/Actor/Organization/OrganizationEdit.jsx
+++ deploy-archipelago-glocal/addOn/frontend/src/resources/Agent/Actor/Organization/OrganizationEdit.jsx
@@ -4,15 +4,16 @@ import {
   SelectInput,
   TabbedForm,
   ImageField,
-  AutocompleteInput,
   BooleanInput,
+  SimpleFormIterator,
+  ArrayInput,
 } from 'react-admin';
 import { ReferenceInput, ImageInput } from '@semapps/input-components';
 import { MarkdownInput } from '@semapps/markdown-components';
-import { MultiLinesInput } from '@semapps/input-components';
-import { OrganizationsInput, EventsInput, ThemesInput, DocumentsInput, LocationInput } from '../../../../common/input';
+import { OrganizationsInput, EventsInput, DocumentsInput, LocationInput } from '../../../../common/input';
 import Edit from "../../../../layout/edit/Edit";
-import ReificationArrayInput from '../../../../common/input/ReificationArrayInput';
+import CustomTreeSelectArrayInput from '../../../../common/input/TreeComponent/CustomTreeSelectArrayInput';
+import MembershipAssociationInput from '../../../../common/input/MembershipAssociationInput';
 
 export const OrganizationEdit = props => (
   <Edit redirect="show" {...props}>
@@ -27,10 +28,11 @@ export const OrganizationEdit = props => (
         <ReferenceInput reference="Type" source="pair:hasType" filter={{ a: 'pair:OrganizationType' }}>
           <SelectInput optionText="pair:label" />
         </ReferenceInput>
-        <BooleanInput source="custom:charterCompliance" label={
-          <div>Cette organisation adhère aux valeurs de la <a href="https://transiscope.org/charte/">charte Transiscope</a></div>
-        } option={{ defaultChecked: true }} />
-        <MultiLinesInput source="pair:homePage" fullWidth />
+        <ArrayInput source="pair:homePage" fullWidth>
+          <SimpleFormIterator disableReordering disableClear fullWidth>
+            <TextInput fullWidth type="url" />
+          </SimpleFormIterator>
+        </ArrayInput>
         <TextInput source="pair:e-mail" fullWidth type="email" />
         <LocationInput source="pair:hasLocation" fullWidth />
         <ImageInput source="image" accept="image/*">
@@ -38,30 +40,20 @@ export const OrganizationEdit = props => (
         </ImageInput>
       </TabbedForm.Tab>
       <TabbedForm.Tab label="Membres">
-        <ReificationArrayInput source="pair:organizationOfMembership" reificationClass="pair:MembershipAssociation">
-          <ReferenceInput reference="Person" source="pair:membershipActor">
-            <AutocompleteInput
-              optionText={record => record && `${record['pair:firstName']} ${record['pair:lastName']}`}
-              shouldRenderSuggestions={value => value && value.length > 1}
-              label="Membre"
-              size="small"
-              sx={{
-                mt: 1,
-                mb: '4px',
-                minWidth: 300,
-              }}
-            />
-          </ReferenceInput>
-          <ReferenceInput reference="MembershipRole" source="pair:membershipRole">
-            <SelectInput optionText="pair:label" label="Rôle" />
-          </ReferenceInput>
-        </ReificationArrayInput>
+        <MembershipAssociationInput
+          source="pair:organizationOfMembership"
+          referenceInputProps={{
+            reference: "Person",
+            source: "pair:membershipActor"
+          }}
+          label="Membre"
+        />
       </TabbedForm.Tab>
       <TabbedForm.Tab label="Relations">
         <OrganizationsInput source="pair:partnerOf" />
         <EventsInput source="pair:involvedIn" />
-        <ThemesInput source="pair:hasTopic" />
         <DocumentsInput source="pair:documentedBy" />
+        <CustomTreeSelectArrayInput source="pair:hasTopic" reference="Theme" label="A pour thème" broader="pair:broader" fullWidth />
       </TabbedForm.Tab>
     </TabbedForm>
   </Edit>
